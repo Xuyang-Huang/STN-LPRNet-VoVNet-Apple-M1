02ac1977fa96e9c58a1f26ff0ba37272644c442b
Xuyang Huang <XHUANG032@e.ntu.edu.sg>
2023-01-27 10:18:21 +0800

    add more stn

--- model/stn.py
+++ model/stn.py
@@ -11,32 +11,34 @@ import torch
 import torch.nn.functional as F
 
 class STN(nn.Module):
-    def __init__(self):
+    def __init__(self, ch_in, ch_out, kernel_size, hw_out):
         super(STN, self).__init__()
         # Spatial transformer localization-network 24x96
-        self.loc_conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1)  # 12x48
-        self.loc_conv2 = nn.Conv2d(32, 32, kernel_size=5, stride=3, padding=1)  # 4x16
-        self.loc_conv3 = nn.Conv2d(3, 32, kernel_size=6, stride=6)  # 4x16
+        self.localization = nn.Sequential(
+            nn.Conv2d(ch_in, (ch_in+ch_out)//2, kernel_size=kernel_size, padding=(kernel_size-1)//2, stride=1),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d((ch_in+ch_out)//2, ch_out, kernel_size=kernel_size, padding=(kernel_size-1)//2, stride=1),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
 
         # Regressor for the 3 * 2 affine matrix
         self.fc_loc = nn.Sequential(
-            nn.Linear(64 * 2 * 32, 32),
-            nn.ReLU(),
+            nn.Linear(ch_out*hw_out[0]*hw_out[1], 32),
+            nn.ReLU(True),
             nn.Linear(32, 3 * 2),
         )
 
     def init_weights(self):
         # Initialize the weights/bias with identity transformation
-        self.fc_loc[0].weight.data.zero_()
         self.fc_loc[2].weight.data.zero_()
         self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
     def forward(self, inputs):
-        x1 = self.loc_conv1(inputs)
-        x1 = self.loc_conv2(x1)
-        x2 = self.loc_conv3(inputs)
+        x = self.localization(inputs)
 
-        x = torch.cat([x1, x2], 1)
+        # x = torch.cat([x1, x2], 1)
         x = torch.flatten(x, 1)
 
         theta = self.fc_loc(x)
